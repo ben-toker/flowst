9c47a65447499c2a383ce9a9d620ad079e6b0cb9
ben-toker <117331544+ben-toker@users.noreply.github.com>
2023-07-07 11:54:48 -0700

    Implemented functioning minutes timer

--- src/timer.rs
+++ src/timer.rs
@@ -1,15 +1,21 @@
 use tokio::time::{sleep,Duration};
 use std::io::{stdout,Write};
 
+fn format_seconds(seconds: u64) -> Vec<u64>{
+       return vec![(seconds/60),(seconds%60)];   
+}
+
+
 //flushes stdout to update the text in realtime.
-pub async fn countdown(seconds: u64) {
+async fn countdown(seconds: u64) {
     let mut stdout = stdout();
-    for i in (0..=seconds).rev() {
-          write!(&mut stdout, "\r{} seconds remaining ",i).unwrap();
+    for i in (0..=(seconds * 60)).rev() {
+        let (minutes,seconds) = (format_seconds(i)[0],format_seconds(i)[1]);
+          write!(&mut stdout,"\r{} minutes and {} seconds remaining ",minutes, seconds).unwrap();
           stdout.flush().unwrap();
           sleep(Duration::from_secs(1)).await;
         }
-    println!("Session complete.");
+    println!("\nSession complete.");
 }
 
 pub async fn start_timer(work: u64, rest: u64) {
