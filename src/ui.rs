8c559be1123c6b96d68e8651f9a6a690ab2a3128
ben-toker <117331544+ben-toker@users.noreply.github.com>
2023-07-17 19:53:27 -0700

    Made small changes to UI.

--- src/ui.rs
+++ src/ui.rs
@@ -4,12 +4,10 @@ use tui::{
     backend::Backend,
     layout::{Alignment, Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    text::Span,
-    widgets::{Block, BorderType, Borders},
+    text::{Span,Spans,Text},
+    widgets::{Block, BorderType, Borders,Paragraph,Wrap},
     Frame, Terminal,
 };
-
-
 
 pub fn chunks<B: Backend>(f: &mut Frame<B>) -> Vec<Rect>{
 let chunks = Layout::default()
@@ -17,13 +15,25 @@ let chunks = Layout::default()
         .margin(1)
         .constraints(
             [
-                Constraint::Percentage(20),
                 Constraint::Percentage(10),
-                Constraint::Percentage(60),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
             ].as_ref()
         )
         .split(f.size());
-chunks
+
+let lower_chunks = Layout::default()
+    .direction(Direction::Horizontal)
+    .margin(1)
+    .constraints(
+        [
+        Constraint::Percentage(20),
+        Constraint::Percentage(80),
+        ].as_ref()
+    )
+    .split(chunks[1]);
+
+vec![chunks[0],lower_chunks[0], lower_chunks[1], chunks[2]]
 }
 
 pub fn tim_display<B: Backend>(f: &mut Frame<B>, tim_msg: &str) {
@@ -33,17 +43,16 @@ pub fn tim_display<B: Backend>(f: &mut Frame<B>, tim_msg: &str) {
          .borders(Borders::ALL);
     f.render_widget(block, chunks[0]);
 
-    // Create a new area for the Paragraph that fits inside the Block.
     let inner_area = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Percentage(100)].as_ref())
-        .margin(1)  // Adjust this value to create a margin inside the Block.
+        .margin(1)  
         .split(chunks[0])[0];
 
     let text = vec![
         tui::text::Spans::from(tui::text::Span::raw(tim_msg)),
     ]; 
-    let paragraph = tui::widgets::Paragraph::new(text).wrap(tui::widgets::Wrap { trim: true });
+    let paragraph = Paragraph::new(text).wrap(Wrap { trim:  true });
     f.render_widget(paragraph, inner_area);
 }
 
@@ -56,12 +65,50 @@ pub fn ui<B: Backend>(f: &mut Frame<B>) {
     //timer logic to be put into block
 
     let block = Block::default()
-         .title("Controls")
-         .borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
-    let block = Block::default()
          .title("Configs")
          .borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
+
+    //Welcome logo
+
+    let block = Block::default()
+         .title("Welcome!")
+         .borders(Borders::ALL);
     f.render_widget(block, chunks[2]);
+
+    let inner_area = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .margin(1)  // Adjust this value to create a margin inside the Block.
+        .split(chunks[2])[0];
+
+    let style = Style::default()
+        .fg(Color::Cyan)
+        .bg(Color::Black);
+
+     let logo = Text::styled(r#"                                                          
+                                                              
+          ,--,                                        ___     
+  .--., ,--.'|                                      ,--.'|_   
+,--.'  \|  | :     ,---.           .---.            |  | :,'  
+|  | /\/:  : '    '   ,'\         /. ./|  .--.--.   :  : ' :  
+:  : :  |  ' |   /   /   |     .-'-. ' | /  /    '.;__,'  /   
+:  | |-,'  | |  .   ; ,. :    /___/ \: ||  :  /`./|  |   |    
+|  : :/||  | :  '   | |: : .-'.. '   ' .|  :  ;_  :__,'| :    
+|  |  .''  : |__'   | .; :/___/ \:     ' \  \    `. '  : |__  
+'  : '  |  | '.'|   :    |.   \  ' .\     `----.   \|  | '.'| 
+|  | |  ;  :    ;\   \  /  \   \   ' \ | /  /`--'  /;  :    ; 
+|  : \  |  ,   /  `----'    \   \  |--" '--'.     / |  ,   /  
+|  |,'   ---`-'              \   \ |      `--'---'   ---`-'   
+`--'                          '---"                           
+"#, style);
+     let paragraph = Paragraph::new(logo).wrap(Wrap{ trim: false});
+    
+    f.render_widget(paragraph, inner_area);
+
+    let block = Block::default()
+         .title("Controls")
+         .borders(Borders::ALL);
+    f.render_widget(block, chunks[3]);
 }
 
